37053d86c2192e8e318503c0fe428f8d1f4ea70e
getzneet <basilegarcia@gmail.com>
2017-10-15 23:43:16 +0200

    add video for each techno (not accurate at ALL and pretty funny)

--- bdat/views.py
+++ bdat/views.py
@@ -1,11 +1,13 @@
+import logging as log
+import urllib.request
+import urllib.parse
+import re
+
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.shortcuts import render_to_response, render
 from .models import Institution, Technology
-import logging as log
 
-
-# Create your views here.
 
 def home(request):
     queryset = Technology.objects.all()
@@ -58,7 +60,9 @@ def categoryt(request):
 
 def technology(request, idx):
     queryset = Technology.objects.all()
+
     techno = [techno for techno in queryset if techno.idx == int(idx)][0]
+    techno.video = get_technology_video(techno.nom)
 
     return render(request, "techno.html",
                   {"att": techno})
@@ -69,3 +73,15 @@ def technology_(request):
 
     return render(request, "techno.html",
                   {"att": queryset[0]})
+
+def get_technology_video(name):
+    """
+    shitty method to get a video describing the techno from youtube
+    (not accurate and resource eating because called at each pageload,
+    maybe a function to call at program startup).
+    """
+
+    query_string = urllib.parse.urlencode({"search_query": name})
+    html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+    search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+    return "http://www.youtube.com/embed/" + search_results[0]
