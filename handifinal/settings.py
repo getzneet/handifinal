204132f57f4767a145630037b136152c27d8aa85
getzneet <basilegarcia@gmail.com>
2017-10-25 15:05:09 +0200

    added windows path in settings.py

--- handifinal/settings.py
+++ handifinal/settings.py
@@ -13,8 +13,14 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import ipgetter
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# If os is windows
+if os.name == "nt":
+    BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+else:
+    # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
